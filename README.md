05e79892042a4110cbb22733ea1a947b9f5faeaa
Gloria Williams <willyabiodun@gmail.com>
2023-12-01 23:28:38 +0100

    Assignment completed with comments included

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Second Assignement at ECX 4.0.
+### Introduction to Javascript
+1. Declared a function named greet that takes the parameter name, console.log was used to print a greeting usung the funcction
+2. Declared a function called multiply that takes two parameters. the console.log prints the multiplication of the numbers 
+3. Declared a function called calculateSquare with a parameter num, the console log returns the square of the given paramter
---
+++ index.js
@@ -0,0 +1,22 @@
+//1. Declared a function named greet that takes the parameter name, console.log was used to print a greeting usung the funcction
+function greet(name) {
+    console.log('hello' + ' ' + name);
+}
+
+greet("Gloria");
+
+
+
+//2. Declared a function called multiply that takes two parameters. the console.log prints the multiplication of the numbers 
+function multiply(num1, num2) {
+console.log(num1 * num2);
+}
+
+multiply(5, 5);
+
+//3. Declared a function called calculateSquare with a parameter num, the console log returns the square of the given paramter
+function calculateSquare(num) {
+console.log(num ** 2 );
+}
+
+calculateSquare(4);
